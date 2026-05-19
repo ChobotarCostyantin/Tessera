03e0991359217dac41bbd2336e446c887f2e5581
ChobotarCostyantin <chobotar.kostiantyn@chnu.edu.ua>
2026-05-19 09:39:03 +0200

    fix: small tweaks

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: false,
+
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/editor',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
